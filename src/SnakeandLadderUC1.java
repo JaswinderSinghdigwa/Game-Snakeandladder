f093f69d0ffb5a8ec768d237de7c11feee0f9c64
Aman <you@example.com>
2021-09-07 19:48:27 +0530

    playerStart

---
+++ src/SnakeandLadderUC1.java
@@ -0,0 +1,13 @@
+
+public class SnakeandLadderUC1 {
+	
+	public String name= "User";
+	public int player =  0;
+	
+}
+
+class ladd2{
+	public static void main(String[] args) {
+		SnakeandLadderUC1 ladd = new SnakeandLadderUC1();
+	}
+}
