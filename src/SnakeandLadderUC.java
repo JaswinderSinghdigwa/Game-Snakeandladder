91712b219aebcb5be8f24e9ac22fadca710d4172
Aman <you@example.com>
2021-09-25 23:59:35 +0530

    cond for ladder,snake and win losstill play 100

--- src/SnakeandLadderUC.java
+++ src/SnakeandLadderUC.java
@@ -1,47 +1,49 @@
 
 public class SnakeandLadderUC {
-
-   	public String name= "User";
-    public int position=0;
-    public int[] ladder = {4,24,48,67,86};
-    public int[] ladderlength= {13,23,5,12,13};
-    public int[] snake = {6,26,47,23,55,97};   
-    public int[] lengthsnake = {4,6,7,5,8,9};
-    
-    
-public void snake() {
-  int  chances = 0;
-    System.out.println("----------------LeTs StArT ThE GaMe----------------\n");
-    while(position <= 100) {
-    	int roll = (int) Math.floor(Math.random()*10) % 6 + 1;
-       System.out.println(+ roll);
-        position = roll + position;
-        if(position > 100) {
-            position = position - roll;
-        }
-        else if(position == 100) {
-            System.out.println("completed the game Winner");
-            break;
-        }
-        for(int n = 0; n < ladder.length; n++) {
-                if (position == ladder[n]) {
-                    position = position + ladderlength[n];
-                }
-        }
-        for (int n = 0; n < snake.length; n++) {
-                if (position == snake[n]) {
-                    position = position - lengthsnake[n];
-                	}
-        }
-
-    }
-}
+	
+	      	public String name= "User";
+	        public int position=0;
+	        public int[] ladder = {4,24,48,67,86};
+	        public int[] ladderlength= {13,23,5,12,13};
+	        public int[] snake = {6,26,47,23,55,97};   
+	        public int[] lengthsnake = {4,6,7,5,8,9};
+	        
+	        
+	    public void snake() {
+	      int  chances = 0;
+	        System.out.println("----------------LeTs StArT ThE GaMe----------------\n");
+	        while(position <= 100) {
+	        	int roll = (int) Math.floor(Math.random()*10) % 6 + 1;
+	           System.out.println(+ roll);
+	            position = roll + position;
+	            if(position > 100) {
+	                position = position - roll;
+	            }
+	            else if(position == 100) {
+	                System.out.println("completed the game");
+	                break;
+	            }
+	            for(int n = 0; n < ladder.length; n++) {
+	                    if (position == ladder[n]) {
+	                        position = position + ladderlength[n];
+	                    }
+	            }
+	            for (int n = 0; n < snake.length; n++) {
+	                    if (position == snake[n]) {
+	                        position = position - lengthsnake[n];
+	                    	}
+	            }
+	            System.out.println("Current position of the player : " +position);
+	            chances += 4/4;
+	        }
+	        System.out.println("ToTal number oF chances : " + chances);
+	   }
 }
 
 class Game{
-public static void main(String[] args) {
-SnakeandLadderUC ladd = new SnakeandLadderUC();
-ladd.snake();	
-}
-
+	public static void main(String[] args) {
+		SnakeandLadderUC ladd = new SnakeandLadderUC();
+		ladd.snake();	
+	}
+	
 }
